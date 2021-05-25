f2555dc6bbb22e7938a15a171c714ce8e49489a4
peachferris <52829920+peachferris@users.noreply.github.com>
2021-05-25 14:14:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const checkbox = document.getElementById('check');
+let btn = document.querySelector('btn')
+const inp = elem.getAttribute(input);
+
+
+function required () {
+    if (checkbox.check , inp.check) {
+        btn.disabled = false
+    } else {
+        btn.disabled = true
+    };
+}
+
