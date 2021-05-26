e882bc0456892ba4fa855bd645576a77c5d4ef3f
peachferris <52829920+peachferris@users.noreply.github.com>
2021-05-26 16:37:33 +0300

    Edit 26.05.2021

--- js/main.js
+++ js/main.js
@@ -1,13 +1,52 @@
+//AJAX
+
+
+
+
+
+
+  
+//Block Button
+
 const checkbox = document.getElementById('check');
-let btn = document.querySelector('btn')
-const inp = elem.getAttribute(input);
+let btn = document.querySelector('.btn');
+btn.setAttribute('disabled', true);
+const inp = document.querySelectorAll('.data-item__input');
 
-
-function required () {
+let required = function() {
     if (checkbox.check , inp.check) {
-        btn.disabled = false
+        btn.disabled = false;
+        console.log('Good');
     } else {
-        btn.disabled = true
-    };
-}
+        btn.disabled = true;
+        console.log('I need more information');
+    }
+};
 
+required();
+/*
+const username = document.getElementById('name');
+
+if(username.lenght > 4) {
+    console.log('Yeah, cool name!');
+    
+} else {
+    console.log('It`s small name!');
+}
+*/
+
+
+//User
+
+const user =  {
+    sum : 1000,
+    numbCard : 2567729381340987, 
+    nameUser : 'Ivan',
+    date : 23092023,
+    cvvCode : 678
+};
+    
+
+
+
+    
