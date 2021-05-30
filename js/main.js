dc9a7a50065fb7dfd2898c8d784e1d0faed51759
peachferris <52829920+peachferris@users.noreply.github.com>
2021-05-30 14:10:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,52 +1,90 @@
 //AJAX
+let getJson = {"balanse": 100000};
+document.getElementById('header-item--balance').innerHTML = getJson.balanse;
 
-
-
-
-
-
-  
-//Block Button
-
-const checkbox = document.getElementById('check');
-let btn = document.querySelector('.btn');
-btn.setAttribute('disabled', true);
-const inp = document.querySelectorAll('.data-item__input');
-
-let required = function() {
-    if (checkbox.check , inp.check) {
-        btn.disabled = false;
-        console.log('Good');
-    } else {
-        btn.disabled = true;
-        console.log('I need more information');
-    }
-};
-
-required();
 /*
-const username = document.getElementById('name');
+const headerBalanse = document.querySelector('.header-item--balance');
+const requestURL = 'js/ajax.js';
+const request = new XMLHttpRequest();
 
-if(username.lenght > 4) {
-    console.log('Yeah, cool name!');
-    
-} else {
-    console.log('It`s small name!');
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    const balanse = request.response;
+    getHeaderBalanse(balanse);
+}
+  
+function getHeaderBalanse(jsonObj) {
+    var myH3 = document.createElement('h3');
+    myH3.textContent = jsonObj['balanse'];
+    headerBalanse.appendChild(myH3);
 }
 */
 
+//Block Button
 
-//User
+    let btn = document.querySelector('.btn');
+    const form = document.querySelector('.data-item__form');
+    const inputs = document.querySelectorAll('input');
+   
+    
 
-const user =  {
-    sum : 1000,
-    numbCard : 2567729381340987, 
+    const getKey = (e) => {
+        let keyNum;
+
+        if(inputs.event) {
+            keyNum.inputs.event.keyUp;
+        } else if (e) {
+                keyNum = e.which;
+        }
+            console.log(keyNum)
+        };
+
+        document.onkeyup = getKey();
+
+   
+    btn.addEventListener('click', () =>{
+        function getCheck(){
+            if (getKey() != "" && check.checked){
+                btn.disabled = true;
+                console.log('Good')
+            } else {
+                btn.disabled = false;
+                console.log('I need more info!')
+            }
+            }
+            getCheck()
+    });
+
+    
+/*
+    btn.addEventListener('click', () => {
+        
+    if (check.checked & inputs.value == "") {
+    
+    btn.disabled = false;
+    inputs.forEach(n => n.addEventListener('input', onInput));
+    onInput();
+    console.log('Good')
+    } else {
+    btn.disabled = true;
+    console.log('I need more info!')
+    }
+    
+    });
+  */  
+    
+
+    
+    
+    //User
+    
+    const user = {
+    sum : 10000,
+    numbCard : 2567729381340987,
     nameUser : 'Ivan',
     date : 23092023,
     cvvCode : 678
-};
-    
-
-
-
-    
+    };
